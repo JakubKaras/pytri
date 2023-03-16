c5a21836a485cf15a63727e51125f2bd1f4790a6
Jakub Karas <karas.kuba@email.cz>
2023-03-16 18:41:22 +0100

    example made nonsymmetrical

--- examples.py
+++ examples.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format = '\n%(asctime)s %(module)s %(levelname)s: %(message)s', datefmt = '%I:%M:%S %p', level = logging.INFO)
 
 def basic_triangulation_dataclass_example():
-    points = [Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1)]
+    points = [Point(0, 0), Point(1, 2), Point(1, 0), Point(2, 2)]
     point_triplets = [Triangle(0, 1, 2), Triangle(1, 2, 3)]
     triangulation = Triangulation(points=points, point_triplets=point_triplets)
     logging.getLogger().info(f"Points:\n{triangulation.points}")
