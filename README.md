9f77c5410f1b3c5a97ee778fcc4589197b0be11e
Jakub Karas <karas.kuba@email.cz>
2023-03-16 18:29:25 +0100

    file with basic examples added with comment in README

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ conda env create -f environment.yml
 conda activate pytri
 ```
 This way you ensure that all important packages are installed.
+
+# Examples
+In the file `examples.py` are couple of runnable examples. Run those any way you see fit, for example by this command:
+```bash
+cd pytri
+python examples.py
+```
---
+++ examples.py
@@ -0,0 +1,15 @@
+import logging
+from data_classes import Triangulation, Point, Triangle
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format = '\n%(asctime)s %(module)s %(levelname)s: %(message)s', datefmt = '%I:%M:%S %p', level = logging.INFO)
+
+def basic_triangulation_dataclass_example():
+    points = [Point(0, 0), Point(0, 1), Point(1, 0), Point(1, 1)]
+    point_triplets = [Triangle(0, 1, 2), Triangle(1, 2, 3)]
+    triangulation = Triangulation(points=points, point_triplets=point_triplets)
+    logging.getLogger().info(f"Points:\n{triangulation.points}")
+    logging.getLogger().info(f"Point triplets:\n{triangulation.point_triplets}")
+
+if __name__ == '__main__':
+    basic_triangulation_dataclass_example()
