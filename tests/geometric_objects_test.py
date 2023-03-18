14035cf6a46620782ebd6415490ae59778d737db
Jakub Karas <karas.kuba@email.cz>
2023-03-18 16:40:38 +0100

    tests for distances added

--- tests/geometric_objects_test.py
+++ tests/geometric_objects_test.py
@@ -1,6 +1,6 @@
 import unittest as ut
 import numpy as np
-from geometric_objects import Point, Triangle, points_to_numpy_array
+from geometric_objects import Point, Triangle, Triangulation, points_to_numpy_array, distance_point_to_point
 
 
 class TestPointClass(ut.TestCase):
@@ -44,3 +44,40 @@ class TestTriangleClass(ut.TestCase):
     def test_list_conversion(self):
         triangle = Triangle(0, 2, 1)
         self.assertEqual(triangle.to_list(), [triangle.point_index1, triangle.point_index2, triangle.point_index3])
+
+class TestDistances(ut.TestCase):
+    def setUp(self) -> None:
+        self.point1 = Point(0, 0)
+        self.point2 = Point(1, 2)
+        self.point3 = Point(1, 0)
+        self.point4 = Point(2, 2)
+        self.triangulation = Triangulation([self.point1, self.point2, self.point3, self.point4], [])
+        self.outside_point = Point(3, 3)
+
+    def test_that_distance_point_to_itself_is_zero(self):
+        for point in self.triangulation.points:
+            self.assertEqual(distance_point_to_point(point, point), 0)
+
+    def test_symmetry_of_point_to_point_distance(self):
+        for i in range(len(self.triangulation.points) - 1):
+            for j in range(i + 1, len(self.triangulation.points)):
+                self.assertEqual(
+                    distance_point_to_point(self.triangulation.points[i], self.triangulation.points[j]),
+                    distance_point_to_point(self.triangulation.points[j], self.triangulation.points[i])
+                )
+
+    def test_triangle_inequality_of_point_to_point_distance(self):
+        for i in range(len(self.triangulation.points) - 2):
+            for j in range(i + 1, len(self.triangulation.points) - 1):
+                for k in range(j + 1, len(self.triangulation.points)):
+                    i_to_j_distance = distance_point_to_point(self.triangulation.points[i], self.triangulation.points[j])
+                    j_to_k_distance = distance_point_to_point(self.triangulation.points[j], self.triangulation.points[k])
+                    i_to_k_distance = distance_point_to_point(self.triangulation.points[i], self.triangulation.points[k])
+                    self.assertGreaterEqual(i_to_j_distance + j_to_k_distance, i_to_k_distance)
+
+    def test_distance_of_triangulations_point_to_the_triangulation_is_zero(self):
+        for point in self.triangulation.points:
+            self.assertEqual(self.triangulation.distance_point_to_triangulation(point), 0)
+
+    def test_distance_to_concrete_point(self):
+        self.assertEqual(self.triangulation.distance_point_to_triangulation(self.outside_point), np.sqrt(2))
